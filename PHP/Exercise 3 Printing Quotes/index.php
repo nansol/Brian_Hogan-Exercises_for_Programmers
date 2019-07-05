7bd14e5fd732c4390fc5c5af5b4b017516c9feb0
nansol <nancysgithub@gmail.com>
2019-07-05 16:36:10 +0200

    Add Style

--- PHP/Exercise 3 Printing Quotes/index.php
+++ PHP/Exercise 3 Printing Quotes/index.php
@@ -5,38 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Printing Quotes</title>
 </head>
 <body>
-<header>
-        <h1>Who said this quote?</h1>
-    </header>
-    <section>
-        <?php
-            $quote="These aren't the droids you're looking for.";
-            $author = "Obi-Wan Kenobi";
+    <div class='container'>
+        <header>
+            <h1 id="title">Who said this quote?</h1>
+        </header>
+        <section>
+            <?php
+                $quote="These aren't the droids you're looking for.";
+                $author = "Obi-Wan Kenobi";
 
-            echo "<h2>$quote</h2>";
-        ?>
-            <form action="" method="POST">
-                <label for="author">Who said it?</label>
-                <input type="text" name="author">
-            </form>
-    </section>
-    <section>
-        <?php
-            if(isset($_POST['author']) == $author){
-                echo "Correct!!!";
+                echo "<div id='question'>
+                        <h2>$quote</h2>
+                      </div>";
+                
+            ?>
+                <form action="" method="POST">
+                    <input id='input' type="text" name="author" placeholder="Enter your answer">
+                </form>
+        </section>
+        <section>
+            <?php
+                if(isset($_POST['author']) == $author){
+                    echo 
+                        "<div id='correct'> Correct!!!</div>";
 
-                echo "<br>";
+                    echo "<br>";
 
-                echo $author . " says: " . " \"	$quote \"	";
-            } 
-            else
-            {
-                echo "Incorrect! Try again";
-            }
-        ?>
-    </section>
+                    echo 
+                    "<div id='quote'>{$author} says: \"	$quote \" </div>";
+                } 
+                else
+                {
+                    echo 
+                    "<div id='incorrect'>Incorrect! Try again</div>";
+        
+                    
+                }
+            ?>
+        </section>
+    </div>
+
 </body>
 </html>
