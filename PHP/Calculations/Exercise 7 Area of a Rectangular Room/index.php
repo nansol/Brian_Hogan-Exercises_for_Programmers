b3e5312a823c7722edee526f7cabed37b50717f5
nansol <nancysgithub@gmail.com>
2019-07-10 12:17:50 +0200

    Create Challenge Exercise

--- PHP/Calculations/Exercise 7 Area of a Rectangular Room/index.php
+++ PHP/Calculations/Exercise 7 Area of a Rectangular Room/index.php
@@ -10,14 +10,12 @@
     <div class='container'>
     <h1>Area of a Rectangular Room</h1>
         <form action="" method='POST'>
-            <input type="number" name='length' placeholder='Lenght?' id='length'required>
-            <input type="number" name='width' placeholder='Width?' id='width' required>
+            <input type="number" name='length' placeholder='Lenght' id='length'required>
+            <input type="number" name='width' placeholder='Width' id='width' required>
             <input type="submit" name='submit'>              
         </form>
 
-        <?php
-
-       
+        <?php   
 
         if(isset($_POST['submit'])){
 
@@ -33,13 +31,57 @@
         echo "The area of a room is " . $area . " square feet or " .  $areaMeter . " square meters."; 
 
         }
-
         ?>
-
-
-
+    	<br>
 
     </div>
+
+
+
+    <!-- Challenge 
+    • Revise the program to ensure that inputs are entered as numeric values. Don’t allow the user to proceed if the value entered is not numeric.
+    • Create a new version of the program that allows you to choose feet or meters for your inputs.
+    • Implement this program as a GUI program that automatically updates the values when any value changes.
+    -->
+
+    <form method='POST'>
+        <select name="dimension">
+            <option value="feets" name='feet'>feet</option>
+            <option value="meters" name='meters'>meters</option>
+        </select>
+        <input type="number" name='length' placeholder='Lenght' id='length'required>
+        <input type="number" name='width' placeholder='Width' id='width' required>
+        <input type="submit" name='sub'>              
+    </form>
+
+    <?php   
+    $selectOption = $_POST['dimension'];
+    $meter = 'meters';
+
+    if(isset($_POST['sub']) && $selectOption == $meter){
+
+        echo "You entered dimensions of " . $_POST['length'] . " meters by " . $_POST['width'] . " meters.";
+    
+        $area = $_POST['length'] * $_POST['width'];
+    
+        echo "<br>";
+    
+        echo "The area of a room is " . $area . " square metters"; 
+    
+        }
+    
+        elseif (isset($_POST['sub']) && $selectOption !== $meter){
+    
+        echo "You entered dimensions of " . $_POST['length'] . " feet by " . $_POST['width'] . " feet.";
+    
+        $area = $_POST['length'] * $_POST['width'];
+    
+        echo "<br>";
+    
+        echo "The area of a room is " . $area . " square feet"; 
+    
+        }
+?>
     
 </body>
 </html>
