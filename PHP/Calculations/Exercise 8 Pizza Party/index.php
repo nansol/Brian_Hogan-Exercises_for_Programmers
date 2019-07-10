9d7f59f0cd20c41c38eb3fad7e95c0fb038ed139
nansol <nancysgithub@gmail.com>
2019-07-10 15:32:00 +0200

    Add Count Function for Pizza Party

--- PHP/Calculations/Exercise 8 Pizza Party/index.php
+++ PHP/Calculations/Exercise 8 Pizza Party/index.php
@@ -7,6 +7,79 @@
     <title>Pizza Party</title>
 </head>
 <body>
+    <div class="container">
+        <form action="" method='POST'>
+        <label for="people">How many people will eat pizza?</label>
+        <input type="number" name="people" min="1" required>
+        <br>
+        <label for="pizza">How many pizzas do you have?</label>
+        <input type="number" name="pizza" min="1" required>
+
+        <input type="submit" name="submit">
+        </form>
+    </div>
+
+
+    <?php
+
+
+        if(isset($_POST['submit']) && (isset($_POST['people'])) && (isset($_POST['pizza']))){
+            
+        $people =  $_POST['people'];
+        $pizza = $_POST['pizza'];
+        $slices = $pizza * 8;
+        $leftover = 0;
+
+        $count = $slices/$people;
+        $slicePerPerson = floor($count);
+        $leftover = ($count - $slicePerPerson) * 10;
+
+        echo $people. ' people with ' . $pizza . ' pizzas';
+        echo '<br>'; 
+        }
+         
+       function slice(){
+                  
+        $people =  $_POST['people'];
+        $pizza = $_POST['pizza'];
+        $slices = $pizza * 8;
+        $count = $slices/$people;
+        $slicePerPerson = floor($count);
+
+        if($slicePerPerson == 1){
+            return $slice = 'slice';
+        } else {
+            return $slice =  "slices";
+        }}
+
+            
+        function leftover()
+
+        {   $people =  $_POST['people'];
+            $pizza = $_POST['pizza'];
+            $slices = $pizza * 8;
+            $leftover = 0;
+           
+            $count = $slices/$people;
+            $slicePerPerson = floor($count);
+            $leftover = ($count - $slicePerPerson) * 10; 
+
+            if($leftover <= 1){
+                return $slice = 'slice';
+            } else {
+                return $slice =  "slices";
+            }
+        }
+
+        echo "Each person gets " . $slicePerPerson . " " .slice() ." of pizza.";
+
+        echo '<br>'; 
+
+        echo "There are ". $leftover . " leftover" . " " .  leftover() . ".";
+    
+
+
+    ?>
     
 </body>
 </html>
