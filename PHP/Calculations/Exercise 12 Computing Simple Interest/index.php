5289b70f9ed8b9ca21e0195bbc18d841957fa5a1
nansol <nancysgithub@gmail.com>
2019-07-14 16:22:37 +0200

    Add Challenge Calculation

--- PHP/Calculations/Exercise 12 Computing Simple Interest/index.php
+++ PHP/Calculations/Exercise 12 Computing Simple Interest/index.php
@@ -20,11 +20,12 @@
             <input type="number" name='years'  step=any  placeholder='Enter the number of years' required>  
             <br>
             <input type="submit" name='submit'>
+            <br>
         </form>
     </div>
 
     <?php
-
+ 
     if (isset ($_POST['submit'])){
 
         $principal = $_POST['principal'];
@@ -44,9 +45,54 @@
         echo "After " .$years . " years at " .$rate . "%, the investment will be worth $". number_format($interest,2) . ".";
 
 
-    }
+    } 
     ?>
 
+    <!-- Challenges
     
+    • Alter the program to use a function called calculateSimpleInterest that takes in the rate, principal, and number
+    of years and returns the amount at the end of the
+    investment.
+    • In addition to printing out the final amount, print out
+    the amount at the end of each year.
+      -->
+
+      <div class='container'>
+        <form action="" method='POST'>
+            <input type="number" name='principal'  step=any  min='1' placeholder='Enter the principal' required>  
+            <br>
+            <input type="number" name='rate'  step=any  placeholder='Enter the rate of interest' required>  
+            <br>
+            <input type="number" name='years'  step=any  placeholder='Enter the number of years' required>  
+            <br>
+            <input type="submit" name='sub'>
+            <br>
+        </form>
+    </div>
+
+    <?php
+    function calculateSimpleInterest(){
+        if (isset ($_POST['sub'])){
+
+            $principal = $_POST['principal'];
+            $rate = $_POST['rate'] ;
+            $years = $_POST['years'];
+            $x = 1; 
+
+     
+            for ($x = 1; $x <= $years; $x++) {
+                 echo $x . " year at " .$rate . "%, the investment will be worth $ " . number_format($principal * (1 + (($rate / 100) * $x)),2) ."<br>";
+            } 
+
+   
+    }}
+    echo calculateSimpleInterest();
+
+
+?>
+
+
+
+
 </body>
 </html>
