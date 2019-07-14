2c816fa71c76fadc72997f12c6346cf90b70dbc2
nansol <nancysgithub@gmail.com>
2019-07-14 14:27:22 +0200

    Add Calculation

--- PHP/Calculations/Exercise 12 Computing Simple Interest/index.php
+++ PHP/Calculations/Exercise 12 Computing Simple Interest/index.php
@@ -8,6 +8,45 @@
     <title>Computing Simple Interest</title>
 </head>
 <body>
+    <header class="container">
+        <h1>Computing Simple Interest</h1>
+    </header>
+     <div class='container'>
+        <form action="" method='POST'>
+            <input type="number" name='principal'  step=any  min='1' placeholder='Enter the principal' required>  
+            <br>
+            <input type="number" name='rate'  step=any  placeholder='Enter the rate of interest' required>  
+            <br>
+            <input type="number" name='years'  step=any  placeholder='Enter the number of years' required>  
+            <br>
+            <input type="submit" name='submit'>
+        </form>
+    </div>
+
+    <?php
+
+    if (isset ($_POST['submit'])){
+
+        $principal = $_POST['principal'];
+        $rate = $_POST['rate'] ;
+        $years = $_POST['years'];
+
+
+        $interest = $principal * (1 + (($rate / 100) * $years));
+
+        echo "Enter the principal: " . $principal;
+        echo "<br>";
+        echo "Enter the rate of interest: " . $rate;
+        echo "<br>";
+        echo "Enter the number of years: " . $years;
+
+        echo "<br>";
+        echo "After " .$years . " years at " .$rate . "%, the investment will be worth $". $interest . ".";
+
+
+    }
+    ?>
+
     
 </body>
 </html>
