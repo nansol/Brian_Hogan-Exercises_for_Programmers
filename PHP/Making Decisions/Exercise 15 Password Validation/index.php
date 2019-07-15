4efda134447c4f6d0dc5dc5209229c3332f6c12a
nansol <nancysgithub@gmail.com>
2019-07-15 11:23:32 +0200

    Create New Folder Exercise 15 and html file

---
+++ PHP/Making Decisions/Exercise 15 Password Validation/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Password Validation</title>
+</head>
+<body>
+    <header class="container">
+        <h1>Password Validatior</h1>
+    </header>
+     <div class='container'>
+        <form action="" method='POST'>
+            <input type="number" name='amount'  step=any  min='1' placeholder='What is the order amount?' required>  
+            <br>
+            <input type="text" name='state'  step=any  placeholder='What is the state?' required>  
+            <br>
+            <input type="submit" name='submit'>
+            <br>
+        </form>
+    </div>
+    
+</body>
+</html>
