22804875ba7594fd01d500e0254332d974f7e01b
nansol <nancysgithub@gmail.com>
2019-07-17 16:37:38 +0200

    Create Html Form

--- PHP/Making Decisions/Exercise 19 BMI Calculator/index.php
+++ PHP/Making Decisions/Exercise 19 BMI Calculator/index.php
@@ -8,6 +8,27 @@
     <title>BMI Calculator</title>
 </head>
 <body>
+    <header class="container">
+        <h1>BMI Calculator</h1>
+    </header>
+     <div class='container'>
+         <form method='POST'>
+            <label for="">Enter your height</label>
+            <input type="number" name='fm' value step=any min="1" placeholder='feet or meter'required>  
+            <input type="number" name='icm' step=any min="1" placeholder='inches or cm' required>  
+            <br>
+            <label for="">Enter your weight</label>
+            <input type="number" name='weight' step=any min="1" placeholder='weight'required>  
+            <br>
+            <input type="radio" name="unit" value="imperial"> Imperial
+            <input type="radio" name="unit" value="metric"> Metric    
+            <br>    
+            <input type="submit" name='submit'>
+            <br>
+        </form>
+
+
+
     
 </body>
 </html>
