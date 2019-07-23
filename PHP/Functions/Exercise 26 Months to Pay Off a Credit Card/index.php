ee7919a217179b92b01c5d76212148114ae5bb21
nansol <nancysgithub@gmail.com>
2019-07-23 15:02:06 +0200

    Create HTML Form

--- PHP/Functions/Exercise 26 Months to Pay Off a Credit Card/index.php
+++ PHP/Functions/Exercise 26 Months to Pay Off a Credit Card/index.php
@@ -7,6 +7,22 @@
     <title>Months to Pay Off a Credit Card</title>
 </head>
 <body>
+    <header class="container">
+        <h1>Months to Pay Off a Credit Card</h1>
+    </header>
+     <div class='container'>
+         <form method='POST'>
+            <input type="number" name='balance' min="1" placeholder='What is your balance?'required>  
+            <br>
+            <input type="number" name='apr' min="1" placeholder='What is the APR on the card (as a percent)?' required>  
+            <br>
+            <input type="number" name='monthlyPayment' min="1" placeholder='What is the monthly payment you can make?' required>  
+            <br>
+            <input type="submit" name='submit'>
+            <br>
+        </form>
+    </div>  
+
     
 </body>
 </html>
