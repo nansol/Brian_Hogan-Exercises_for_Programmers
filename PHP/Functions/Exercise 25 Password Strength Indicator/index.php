17ed7c22438f815a3b7b093f870291c9b94b06ac
nansol <nancysgithub@gmail.com>
2019-07-23 18:17:33 +0200

    Fix Regex Condition, not working properly

--- PHP/Functions/Exercise 25 Password Strength Indicator/index.php
+++ PHP/Functions/Exercise 25 Password Strength Indicator/index.php
@@ -29,28 +29,28 @@
         $specialChars = preg_match('@[^\w]@', $password);
         //(?=\S{8,})
 
-        //A very weak password contains only numbers and is fewer than eight characters
-        if($number && strlen($password) < 8){
-            echo "The password ".  $password  ." is a very weak password.";
+    
+            //A very weak password contains only numbers and is fewer than eight characters
+            if($number){
+                echo "The password ".$password  ." is a very weak password.";
+            }
+    
+            //A weak password contains only letters and is fewer than eight characters.
+            elseif($uppercase || $lowercase && !$number){
+            echo "The password ". $password  ." is a weak password.";
+            } 
+            
+            //A strong password contains letters and at least one number and is at least eight characters.
+            elseif($uppercase && $lowercase && $number){
+            echo "The password ". $password  ." is a strong password.";
+            }
+            //A very strong password contains letters, numbers, and special characters and is at least eight characters.
+            elseif($uppercase && $lowercase && $number && $specialChars ){
+            echo "The password ". $password  ." is a strong password.";
+            }
         }
-
-        //A weak password contains only letters and is fewer than eight characters.
-        elseif(strlen($password) < 8 && $uppercase || $lowercase){
-        echo "The password ". $password  ." is a weak password.";
-        }
-
-        //A strong password contains letters and at least one number and is at least eight characters.
-        elseif(strlen($password) <= 8 && $uppercase && $lowercase && $number){
-        echo "The password ". $psw  ." is a strong password.";
-        }
-        //A very strong password contains letters, numbers, and special characters and is at least eight characters.
-        elseif(strlen($password) <= 8 && $uppercase && $lowercase && $number && $specialChars ){
-        echo "The password ". $psw  ." is a strong password.";
-        }
-
-  
-       
-    }
+ 
+    
     ?>
 
     
