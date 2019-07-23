6c0fdc3a8c8af49d910a6835d2517b4f4e304433
nansol <nancysgithub@gmail.com>
2019-07-23 16:01:38 +0200

    Add PHP calculation

--- PHP/Functions/Exercise 26 Months to Pay Off a Credit Card/index.php
+++ PHP/Functions/Exercise 26 Months to Pay Off a Credit Card/index.php
@@ -24,6 +24,30 @@
         </form>
     </div>  
 
+    <?php
+    if(isset($_POST['submit'])){
+        $balance = $_POST['balance'];
+        $apr = $_POST['apr']/100;
+        $monthlyPayment = $_POST['monthlyPayment'];
+        $i =  $apr/365;
+
+        $pow = pow(1+$i,30);
+        $a =1 - $pow;
+        $l = log(1+($balance/$monthlyPayment) * $a);
+        $log = $l / log(1 + $i);
+        $n = -(1/30) * $log;
+
+        echo "Your balance is: " . $balance ."<br>";
+        echo "Apr on the card is: " . $_POST['apr']. "%"."<br>";
+        echo "You can montly pay: "  ."\$".$monthlyPayment."<br>";
+        echo "It will take you " .ceil($n). " months to pay off this card.";
+
+
+
+}
+
+    ?>
+
     
 </body>
 </html>
