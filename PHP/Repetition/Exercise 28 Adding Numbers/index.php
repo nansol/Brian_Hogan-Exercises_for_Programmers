3f7cdc682c102b738757e9422fbfe1aa242cb436
nansol <nancysgithub@gmail.com>
2019-07-24 17:03:14 +0200

    Add For loop and Calculation

--- PHP/Repetition/Exercise 28 Adding Numbers/index.php
+++ PHP/Repetition/Exercise 28 Adding Numbers/index.php
@@ -8,6 +8,31 @@
     <title>Adding Numbers</title>
 </head>
 <body>
-    
+    <header class="container">
+        <h1>Adding Numbers</h1>
+    </header>
+    <form method = "POST">
+        <?php
+            for ($i = 0; $i < 5; $i++) {
+                echo "<input type='number' placeholder='Enter a number' name='input[]'". $i." /><br>";
+            }
+        ?>
+        <input type="submit" name='submit'>
+    </form>
+
+    <?php
+    if(isset($_POST['submit'])){
+        $i = $_POST['input'];
+        $total = 0;
+
+    foreach($i as $value){
+        echo "Enter a number: " .$value ."<br>";
+        
+        $total+=$value;
+    }
+        echo "The total is " .$total.".";
+    }
+    ?>
+
 </body>
 </html>
