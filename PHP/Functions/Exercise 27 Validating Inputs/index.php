65bb0dd42073479604a3c09f4ff34ed9eaa05a77
nansol <nancysgithub@gmail.com>
2019-07-24 11:06:57 +0200

    Add HTML Form

--- PHP/Functions/Exercise 27 Validating Inputs/index.php
+++ PHP/Functions/Exercise 27 Validating Inputs/index.php
@@ -8,6 +8,26 @@
     <title>Validating Inputs</title>
 </head>
 <body>
+    <header class="container">
+        <h1>Validating Inputs</h1>
+    </header>
+    <div class='container'>
+         <form method='POST'>
+            <input type="text" name='name' pattern="[A-Za-z]{2,}" placeholder='Enter your first name'required>  
+            <br>
+            <input type="text" name='surname' pattern="[A-Za-z]{2,}" placeholder='Enter your last name'required>  
+            <br>
+            <input type="text" name='id' pattern="[A-Z]{1}[A-Z]{1}\-{1}[0-9]{4}" placeholder='Enter your employee ID'required>  
+            <br>
+            <input type="number" name='zip' placeholder='Enter your ZIP code'required>  
+            <br>
+            <input type="submit" name='submit'>
+            <br>
+        </form>
+    </div>  
+
+
+
     
 </body>
 </html>
