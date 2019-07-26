8d83bd26baf4f656b6231086cec96123ce4ff7ea
nansol <nancysgithub@gmail.com>
2019-07-26 15:37:59 +0200

    Create Html Form

--- PHP/Repetition/Experience 31 Karvonen Heart Rate/index.php
+++ PHP/Repetition/Experience 31 Karvonen Heart Rate/index.php
@@ -8,6 +8,21 @@
     <title>Karvonen Heart Rate</title>
 </head>
 <body>
-    
+    <header class="container">
+        <div class='row text-center'>
+            <div class='col mt-5'>
+                <h1>Karvonen Heart Rate</h1>
+            </div>
+        </div>
+    </header>
+    <form method="POST">
+        <input type="number" name='age'min="1" placeholder="Enter your age" required>
+        <br>
+        <input type="number" name='rate' min="1" placeholder="Enter your resting heart rate" required>
+        <br>
+        <input type="submit" name="submit">
+    </form>
+
+
 </body>
 </html>
