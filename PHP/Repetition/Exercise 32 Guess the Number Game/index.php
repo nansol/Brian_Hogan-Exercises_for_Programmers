720035c231d0e20810a78d6da6c5da40c2d4b182
nansol <nancysgithub@gmail.com>
2019-07-27 16:36:07 +0200

    Add Guess function for level 1

--- PHP/Repetition/Exercise 32 Guess the Number Game/index.php
+++ PHP/Repetition/Exercise 32 Guess the Number Game/index.php
@@ -15,17 +15,41 @@
             </div>
         </div>
     </header>
-    <p>Let's play Guess the Number</p>
     <form method="POST">
         <label for="level">Pick a difficulty level (1, 2, or 3)</label>
-        <select name="level">
+        <select name="level[]">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
         </select>
-        <br>
+        <br>    
         <input id="guess" type="number" name='guess' min='1' max='10' placeholder="I have my number. What's your guess?">
+        <br>
+        <input type = "submit" name = "submit" value='submit'>
     </form>
+    <?php
+     if(isset($_POST['submit'])){
+        function level1(){
+            $guess = $_POST['guess'];
+            $random = rand(1,10);
+
+            if($random == $guess)
+            {
+                echo 'Correct! The guessed number was ' .$random;
+            }
+            elseif($random < $guess)
+            {
+                echo "Too low. Guess again: " .$guess;
+            }
+            elseif($random > $guess)
+            {
+                echo "Too high. Guess again: " .$guess;
+            }       
+
+        }
+        echo level1();
+    }
+    ?>
     
 </body>
 </html>
