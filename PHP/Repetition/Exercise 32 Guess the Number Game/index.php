6fae7015c5a3dab3abbca98249f1680c73281fc3
nansol <nancysgithub@gmail.com>
2019-07-27 15:03:02 +0200

    Add HTML Form

--- PHP/Repetition/Exercise 32 Guess the Number Game/index.php
+++ PHP/Repetition/Exercise 32 Guess the Number Game/index.php
@@ -8,6 +8,24 @@
     <title>Guess the Number Game</title>
 </head>
 <body>
+    <header class="container">
+        <div class='row text-center'>
+            <div class='col mt-5'>
+                <h1>Guess the Number Game</h1>
+            </div>
+        </div>
+    </header>
+    <p>Let's play Guess the Number</p>
+    <form method="POST">
+        <label for="level">Pick a difficulty level (1, 2, or 3)</label>
+        <select name="level">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>
+        <br>
+        <input id="guess" type="number" name='guess' min='1' max='10' placeholder="I have my number. What's your guess?">
+    </form>
     
 </body>
 </html>
