f4ccf306db52b0b987dbec01f68c4e95a1553cd3
nansol <nancysgithub@gmail.com>
2019-07-27 17:52:46 +0200

    Add PHP Function, not working properly yet

--- PHP/Repetition/Exercise 32 Guess the Number Game/index.php
+++ PHP/Repetition/Exercise 32 Guess the Number Game/index.php
@@ -17,18 +17,33 @@
     </header>
     <form method="POST">
         <label for="level">Pick a difficulty level (1, 2, or 3)</label>
-        <select name="level[]">
+        <select name="level">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
         </select>
         <br>    
-        <input id="guess" type="number" name='guess' min='1' max='10' placeholder="I have my number. What's your guess?">
-        <br>
-        <input type = "submit" name = "submit" value='submit'>
-    </form>
-    <?php
-     if(isset($_POST['submit'])){
+    <?php     
+    if(isset($_POST['level'])){
+        $level = $_POST['level'];
+      
+        if($level == '1'){
+            echo "<input id='guess' type='number' name='guess' min='1' max='10' placeholder='I have my number. What's your guess?' required>";
+        }
+        elseif($level == '2'){
+            echo "<input id='guess' type='number' name='guess' min='1' max='100' placeholder='I have my number. What's your guess?' required>";
+        }
+        else{
+            echo "<input id='guess' type='number' name='guess' min='1' max='1000' placeholder='I have my number. What's your guess?' required>";
+        } 
+    }
+    ?>
+    <input type="submit" name='submit'>
+     </form>
+
+     <?php
+     if(isset($_POST['guess'])){
+   
         function level1(){
             $guess = $_POST['guess'];
             $random = rand(1,10);
@@ -47,9 +62,58 @@
             }       
 
         }
-        echo level1();
-    }
+
+        function level2(){
+            $guess = $_POST['guess'];
+            $random = rand(1,100);
+
+            if($random == $guess)
+            {
+                echo 'Correct! The guessed number was ' .$random;
+            }
+            elseif($random < $guess)
+            {
+                echo "Too low. Guess again: " .$guess;
+            }
+            elseif($random > $guess)
+            {
+                echo "Too high. Guess again: " .$guess;
+            }       
+
+        }
+
+        function level3(){
+            $guess = $_POST['guess'];
+            $random = rand(1,1000);
+
+            if($random == $guess)
+            {
+                echo 'Correct! The guessed number was ' .$random;
+            }
+            elseif($random < $guess)
+            {
+                echo "Too low. Guess again: " .$guess;
+            }
+            elseif($random > $guess)
+            {
+                echo "Too high. Guess again: " .$guess;
+            }       
+
+        }
+
+        if($level == '1'){
+            echo level1();
+        }
+        elseif($level == '2'){
+            echo level2();
+        }
+        else{
+            echo level3();
+        }
+       }
+
+       
     ?>
-    
+
 </body>
 </html>
