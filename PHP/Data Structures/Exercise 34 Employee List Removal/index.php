c97f1b041c977276d49a5da1ddea9b7fe8f9cac7
nansol <nancysgithub@gmail.com>
2019-07-31 09:24:39 +0200

    Create HTML form

--- PHP/Data Structures/Exercise 34 Employee List Removal/index.php
+++ PHP/Data Structures/Exercise 34 Employee List Removal/index.php
@@ -8,6 +8,20 @@
     <title>Employee List Removal</title>
 </head>
 <body>
+    <header class="container">
+        <h1>Employee List Removal</h1>
+    </header>
+    <div class='container'>
+        <p>There are 5 Employees</p>
+        <form method="POST">
+            <label for="input">Enter an employee name to remove</label>
+            <br>
+            <input id='input' type="text" name='input' required>
+            <br>
+            <input type="submit" name='submit'>
+        </form>
+    </div>
+
     
 </body>
 </html>
