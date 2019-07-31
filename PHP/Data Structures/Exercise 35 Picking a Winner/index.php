98d0ea266cd919851fb0e1331b41fae6994f6c4d
nansol <nancysgithub@gmail.com>
2019-07-31 19:26:15 +0200

    Add HTml form

--- PHP/Data Structures/Exercise 35 Picking a Winner/index.php
+++ PHP/Data Structures/Exercise 35 Picking a Winner/index.php
@@ -8,6 +8,30 @@
     <title>Picking a Winner</title>
 </head>
 <body>
-    
+    <header class="container">
+        <h1>Picking a Winner</h1>
+    </header>
+    <div class='container'>
+         <form method="POST">
+            <label for="input">Enter your name</label>
+            <br>
+            <?php 
+            for($i=1; $i<5; $i++)
+            {
+                echo "<input id='input' type='text' name='input[]'.$i . required><br>";
+            }
+            ?>
+            <br>
+            <input type="submit" name='submit'>
+        </form>
+    </div>
+    <?php
+    if(isset($_POST['submit'])){      
+    $input = $_POST['input'];
+
+
+      
+  }
+    ?>
 </body>
 </html>
