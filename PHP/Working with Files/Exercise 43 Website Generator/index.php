cd86e3e852b72301fc8fe96fa244066ad9a19aac
nansol <nancysgithub@gmail.com>
2019-08-10 17:07:08 +0200

    Add HTML form

--- PHP/Working with Files/Exercise 43 Website Generator/index.php
+++ PHP/Working with Files/Exercise 43 Website Generator/index.php
@@ -8,6 +8,28 @@
     <title>Website Generator</title>
 </head>
 <body>
+    <header class='container'>
+        <h1>Website Generator</h1>
+    </header>
+    <div class='container'>
+        <form method="POST">
+            <input type="text" name='name' placeholder='Site name' required>
+            <br>
+            <input type="text" name='author' placeholder='Site author' required>
+            <br>
+            <label for="js">Do you want a folder for JavaScript?</label>
+            <input type="radio" name='js' value='yes'>YES
+            <input type="radio" name='js' value='no' >NO
+            <br>
+            <label for="js">Do you want a folder for CSS?</label>
+            <input type="radio" name='css' value='yes'>YES
+            <input type="radio" name='css' value='no'>NO
+            <br>
+            <input type="submit" name='submit'>
+        </form>
+    </div>
+
+ 
     
 </body>
 </html>
