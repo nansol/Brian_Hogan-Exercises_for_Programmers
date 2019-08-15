2b9b9cf13aa82b0180fe33c37324582ae3c6019e
nansol <nancysgithub@gmail.com>
2019-08-15 16:14:35 +0200

    Add new product to json array

--- PHP/Working with Files/Exercise 44 Product Search/index.php
+++ PHP/Working with Files/Exercise 44 Product Search/index.php
@@ -1,20 +1,9 @@
 <?php
-
 // Read JSON file
 $json = file_get_contents('./products.json');
 
 //Decode JSON
 $json_data = json_decode($json,true);
-foreach ($json_data['products'] as $key=>$value){
-    if($value['name'] == 'Widget')
-    {
-        echo 'Name: '.$value['name'].'<br>';
-        echo 'Price: '.$value['price'].'<br>';
-        echo 'Quantity on hand:'.$value['quantity'].'<br>';
-
-    }
-    }
-
 ?>
 
 <!DOCTYPE html>
@@ -30,6 +19,61 @@ foreach ($json_data['products'] as $key=>$value){
     <header class='container'>
         <h1>Product Search</h1>
     </header>
+    <form method="POST">
+    <input type="text" name='input' placeholder='What is the product name?'required>
+    <input type="submit" name='submit'>
+    </form>
+    <?php
+    if(isset($_POST['submit'])){
+        $i = $_POST['input'];
+        $input = ucfirst($i);
+
+
+         foreach ($json_data['products'] as $key=>$value){
+            if($value['name'] == $input)
+            {
+                echo 'Name: '.$value['name'].'<br>';
+                echo 'Price: $'.$value['price'].'<br>';
+                echo 'Quantity on hand:'.$value['quantity'].'<br>';break 1;
+            }
+            elseif($value['name'] !== $input){
+                echo "Sorry, that product was not found in our inventory.<br>";
+                echo "
+                <form method='POST'>
+                <label for='add'>Do you want add product to the list?</label>
+                <input type='radio' name='add' value='yes'>Yes
+                <input type='radio' name='add' value='no'>No
+                <br>
+                <input type='submit' name='s'>
+                </form>";break 1;    
+            }
+     }
+
+}
+
+if(isset($_POST['s']) && $_POST['add'] == 'yes')   {
+    echo "
+    <form method='POST'>
+    <input type='text' name='name' placeholder='Add Product Name'>
+    <input type='text' name='price' placeholder='Add Product Price'>  
+    <input type='text' name='quantity' placeholder='Add Product Quantity'>
+    <input type='submit' name='sub'>
+    </form>";    
+ }
+
+
+if(isset($_POST['sub'])){
+    $n = $_POST['name'];
+    $name = ucfirst($n);
+    $price = $_POST['price'];
+    $quantity = $_POST['quantity'];
+
+    $json_data [] = ['name'=>$name, 'price'=>$price, 'quantity'=>$quantity];
+    $json = json_encode($json_data);
+
+
+}
+    ?>
     
 </body>
 </html>
