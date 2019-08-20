85d16e7419fd5d013531be20f12d2054d8423323
nansol <nancysgithub@gmail.com>
2019-08-20 11:26:06 +0200

    Add Json file and loop to output data

--- PHP/Working with External Services/Exercise 47 Who is in Space/index.php
+++ PHP/Working with External Services/Exercise 47 Who is in Space/index.php
@@ -1,3 +1,14 @@
+<?php
+// Read JSON file
+$json = file_get_contents('http://api.open-notify.org/astros.json');
+
+//Decode JSON
+$json_data = json_decode($json,true);
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +19,26 @@
     <title>Who’s in Space?</title>
 </head>
 <body>
-    
+    <header class='container'>
+        <h1>Who’s in Space?</h1>
+    </header>
+    <?php
+    echo "There are ";
+    echo $json_data["number"];
+    echo " people in space right now:"
+    ?>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Craft</th> 
+        </tr>
+    <?php foreach($json_data['people'] as $key=>$value): ?>
+        <tr>
+            <td><?php echo $value['name']; ?></td>
+            <td><?php echo $value['craft']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
+
 </body>
 </html>
