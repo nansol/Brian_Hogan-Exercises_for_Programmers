53031f9b39521c13a74a35544eaff597593a9df1
nansol <nancysgithub@gmail.com>
2019-08-20 16:43:38 +0200

    Add timezone to sunset and sunrise

--- PHP/Working with External Services/Exercise 48 Grabbing the Weather/index.php
+++ PHP/Working with External Services/Exercise 48 Grabbing the Weather/index.php
@@ -26,18 +26,23 @@ require "api.php";
         $weatherJson = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".$city."&appid=".$apiKey);
         $weather = json_decode($weatherJson, true);
 
+      //  print_r($weather);    
+
         $temCel = $weather['main']['temp'] -273.15;
         $description = $weather['weather'][0]['description'];
         $humidity = $weather['main']['humidity']." %";
+        $timezone = $weather['timezone'];
 
-        $sunrise = $weather['sys']['sunrise'];
-        $sunset = $weather['sys']['sunset'];
+        $sunrise = $weather['sys']['sunrise'] +($timezone) ;
+        $sunset = $weather['sys']['sunset']+($timezone) ;
 
 
+
+       
         echo "<h2>".$city." weather today:</h2>";
         echo "<p>".$temCel. " &deg;C<br> 
-                   Sunrise at ".date("H:i", $sunrise)."<br>
-                   Sunset at ".date("H:i", $sunset).        
+                   Sunrise at ".gmdate ("H-i", $sunrise)."<br>
+                   Sunset at ".gmdate ("H-i", $sunset).        
              "</p>";
         echo "<small> Description: ".$description."</small><br>";
         echo "<small> Humidity: ".$humidity."</small><br>";
