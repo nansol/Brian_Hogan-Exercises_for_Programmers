7237f62fa5da25a30737402c8cf937edfa1062de
nansol <nancysgithub@gmail.com>
2019-08-21 16:23:11 +0200

    Create Photo Galley by API Flickr

--- PHP/Working with External Services/Exercise 49 Flickr Photo Search/index.php
+++ PHP/Working with External Services/Exercise 49 Flickr Photo Search/index.php
@@ -1,3 +1,6 @@
+<?php
+require "api.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,13 +11,32 @@
     <title>Flickr Photo Search </title>
 </head>
 <body>
-    <header>
+    <header class='container'>
         <h1>Flickr Photo Search</h1>
+        <a href=""></a>
     </header>
     <form method="GET">
         <input type="text" name='tag' placeholder="Photos, people or groups">
     </form>
-   
+    <?php
+    if(isset($_GET['tag'])){
+        $tag = $_GET['tag'];
+        $url = file_get_contents("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=e9f40d8d766ea3b570f07debfd7c2844&tags=.'$tag'.&format=json&nojsoncallback=1");
+        $api = json_decode($url, true);
+        $pictures = $api['photos']['photo'];
+
+  
+        foreach($pictures as $photo){
+            echo 
+            '<div class="gallery">
+              <img class="link" src="http://farm'.$photo['farm'].'.staticflickr.com/'.$photo['server'].'/'.$photo['id'].'_'.$photo['secret'].'_t.jpg">
+            </div>';
+
+
+        }
+        
+    }
+    ?>
     
 </body>
 </html>
