4bbc4d96743233a10dce953bf3a40a3c6485c02d
nansol <nancysgithub@gmail.com>
2019-08-22 18:26:13 +0200

    Add Api key and loop thru json array

--- PHP/Working with External Services/Exercise 50 Movie Recommendations/index.php
+++ PHP/Working with External Services/Exercise 50 Movie Recommendations/index.php
@@ -1,3 +1,11 @@
+<?php
+require "api.php";
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +16,43 @@
     <title>Movie Recommendations</title>
 </head>
 <body>
+    <header>
+        <h1>Movie Database</h1>
+    </header>
+    <form method="GET">
+        <input type="text" name="search" placeholder="title, artist, director"> 
+    </form>
+    <?php
+    if(isset($_GET['search'])){
+        $search = $_GET['search'];
+
+
+        $link = file_get_contents("https://api.themoviedb.org/3/search/multi?include_adult=false&page=1&language=en-US&api_key=f9e9438604faec2c545a1d71f3134e3a&query=.'$search'.");
+                                  
+
+        $url = json_decode($link,true);
+        
+        //print_r($url);
+        foreach($url['results'] as $key=>$value){
+           $pic = $value['poster_path'];
+            echo 
+            '<div class="gallery">
+              <img class="link" src="https://image.tmdb.org/t/p/w500/.$pic.jpg">
+            </div>';   
+         echo $value['title'].'<br>';
+         echo $value ['release_date'].'<br>';
+         echo "Plot: ". $value ['overview'].'<br>';
+
+
+        }
+        
+
+
+       
+    }
+    
+    
+    ?>
     
 </body>
 </html>
