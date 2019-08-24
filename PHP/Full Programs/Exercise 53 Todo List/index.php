f984d85fe9ac1f612cd9da0d2b24b3f672b2364f
nansol <nancysgithub@gmail.com>
2019-08-24 15:43:38 +0200

    Connect SQL database and Forloop item from task table

--- PHP/Full Programs/Exercise 53 Todo List/index.php
+++ PHP/Full Programs/Exercise 53 Todo List/index.php
@@ -1,3 +1,9 @@
+<?php
+include "sql.php";
+print_r($todo);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +14,49 @@
     <title>Todo List</title>
 </head>
 <body>
+    <header>
+        <h1>ToDo List</h1>
+    </header>
+    <form method="POST">
+        <input id='input' type="text" name="task" placeholder="Add your task" required>
+    </form>
+    <ul>
+    <?php foreach($todo as $key=>$value) :?>{
+        <li> <?php echo $value['task'];?> 
+            <button name='update'>Update</button>
+            <button name='delete'>Delete</button>
+        </li>
+    }   
+    <?php endforeach; ?>
+
+    </ul>
+
+    <?php
+
+    if(isset($_POST['task']))
+        {
+            $task = $_POST['task'];
+            
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            } 
+
+            $sql = "INSERT INTO tasks (task)
+            VALUES ('$task')";
+
+            if ($conn->query($sql) === TRUE) {
+            //echo " New record created successfully";
+            } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+
+            $conn->close();
+
+            }
+    ?>
     
 </body>
 </html>
