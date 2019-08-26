977ffff1ac178de55e2d7f006449fcf00004f29f
nansol <nancysgithub@gmail.com>
2019-08-26 19:18:45 +0200

    Create DB and INSERT input into DB

--- PHP/Applications/Exercise 54 URL Shortener/index.php
+++ PHP/Applications/Exercise 54 URL Shortener/index.php
@@ -1,6 +1,8 @@
+<?php
+include "sql.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,9 +10,21 @@
     <link rel="stylesheet" href="style.css">
     <title>URL Shortener</title>
 </head>
-
 <body>
-
+    <header class='container'>
+        <h1>Url Shortener</h1>
+    </header>
+    <div class='container'>
+        <form method="GET">
+            <input type="url" name='url'placeholder='Add URL' required>
+        </form>
+    </div>
+    <?php
+    if(isset($_GET['url'])){
+        $urlInput = $_GET['input'];
+       // $urlShort = 
+    }
+    ?>
 </body>
 
 </html>
