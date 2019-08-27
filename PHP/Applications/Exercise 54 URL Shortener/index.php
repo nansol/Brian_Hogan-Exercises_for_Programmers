e3742244cada338c0d236cd7bbabf2144110e9de
nansol <nancysgithub@gmail.com>
2019-08-27 16:33:07 +0200

    Insert URL to Database

--- PHP/Applications/Exercise 54 URL Shortener/index.php
+++ PHP/Applications/Exercise 54 URL Shortener/index.php
@@ -24,6 +24,33 @@ include "sql.php";
         $urlInput = $_GET['url'];
         $shuffle = str_shuffle($urlInput);
         $urlShort = "nan.cy/".substr($shuffle, -4);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $query = "SELECT * FROM url WHERE urlInput = $urlInput";
+    $result = $conn->query($query);
+
+    if($result->num_rows > 0)
+    {
+        echo "The URL is already in the database";
+    }
+    else{
+        $sql = "INSERT INTO url (urlInput, urlShort)
+        VALUES ('$urlInput', '$urlShort')";
+    
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+ 
+
+    $conn->close();    
 
 
 
