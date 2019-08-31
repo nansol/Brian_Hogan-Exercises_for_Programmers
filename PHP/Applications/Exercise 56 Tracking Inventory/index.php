2c96159aa1dda70838b99b20434c0d7038d3e95c
nansol <nancysgithub@gmail.com>
2019-08-31 14:44:51 +0200

    Add Html form

--- PHP/Applications/Exercise 56 Tracking Inventory/index.php
+++ PHP/Applications/Exercise 56 Tracking Inventory/index.php
@@ -7,7 +7,21 @@
     <link rel="stylesheet" href="style.css">
     <title>Tracking Inventory</title>
 </head>
+    <header>
+        <h1>Tracking Inventory</h1>
+    </header>
 <body>
-    
+    <form method="POST">
+        <input type="text" name='name' placeholder="Name" required>
+        <br>
+        <input type="number" name='serialNum' step="1" placeholder="Serial Number" required>
+        <br>
+        <input type="text" name='value' placeholder="Value" required>
+        <br>
+        <label for="image">Inventory Image</label>
+        <input type="file" name='image'>
+        <br>
+        <input type="submit" name='submit'>
+    </form>
 </body>
 </html>
